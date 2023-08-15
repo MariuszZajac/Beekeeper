c47d2474e6e7d056c2d391cf4f9ccfc6510d0bea
Mariusz Zając <zajac@gmx.us>
2023-08-15 16:01:13 +0200

    Initial Commit

---
+++ Beekeeper/BeekeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeekeeperApp.swift
+//  Beekeeper
+//
+//  Created by Mariusz Zając on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeekeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
