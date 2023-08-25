2eda257a5b4f0642efa35eb88b44cd3fd2acde4d
Mariusz Zając <zajac@gmx.us>
2023-08-25 11:32:26 +0200

    hive create cd..

--- Beekeeper/View/Hive/CreateHiveView.swift
+++ Beekeeper/View/Hive/CreateHiveView.swift
@@ -8,35 +8,24 @@
 import SwiftUI
 
 struct CreateHiveView: View {
-    @State var hiveNumber: Int
-    @State var selectedProducent: HiveProducent
-    @State private var hiveSections: [HiveSection: Int] = [
-        .nest: 1,
-        .body: 0,
-        .honeySuper: 0,
-        .feeder: 0
-    ]
-    @State private var showingAlert = false
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
+    @ObservedObject var viewModel = CreateHiveViewModel()
     
     var body: some View {
         VStack(spacing: 5) {
-            TextField("Numer ula", value: $hiveNumber, formatter: NumberFormatter())
+            TextField("Numer ula", value: $viewModel.hiveNumber, formatter: NumberFormatter())
                 .keyboardType(.numberPad)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .frame(width: 180)
             
-            Picker("Wybierz Producenta", selection: $selectedProducent) {
+            Picker("Wybierz Producenta", selection: $viewModel.selectedProducent) {
                 ForEach(HiveProducent.allCases, id: \.self) { producent in
                     Text(producent.rawValue).tag(producent)
                 }
             }
-            VStack(alignment: .leading,spacing: 5) {
+            VStack(alignment: .leading, spacing: 5) {
                 Text("Skład ula:")
                     .font(.title3)
                     .bold()
-                
                 
                 ForEach(HiveSection.allCases, id: \.self) { section in
                     HStack(spacing: 5) {
@@ -46,19 +35,20 @@ struct CreateHiveView: View {
                         
                         Button(action: {
                             if section == .nest {
-                                if hiveSections[section, default: 0] > 1 {
-                                    hiveSections[section, default: 0] -= 1
+                                if viewModel.hiveSections[section, default: 0] > 1 {
+                                    viewModel.hiveSections[section, default: 0] -= 1
                                 }
-                            } else if hiveSections[section, default: 0] > 0 {
-                                hiveSections[section, default: 0] -= 1
+                            } else if viewModel.hiveSections[section, default: 0] > 0 {
+                                viewModel.hiveSections[section, default: 0] -= 1
                             }
                         }) {
                             Image(systemName: "minus.circle")
-                            .foregroundColor(.red)                        }
-                        Text("\(hiveSections[section, default: 0])")
+                                .foregroundColor(.red)
+                        }
+                        Text("\(viewModel.hiveSections[section, default: 0])")
                             .bold()
                         Button(action: {
-                            hiveSections[section, default: 0] += 1
+                            viewModel.hiveSections[section, default: 0] += 1
                         }) {
                             Image(systemName: "plus.circle")
                                 .foregroundColor(.green)
@@ -66,67 +56,17 @@ struct CreateHiveView: View {
                     }
                 }
             }
-            Button("Utwórz ul", action: createHive)
+            Button("Utwórz ul", action: viewModel.createHive)
                 .buttonStyle(FilledRoundedCornerButtonStyle())
-                .alert(isPresented: $showingAlert) {
-                    Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                .alert(isPresented: $viewModel.showingAlert) {
+                    Alert(title: Text(viewModel.alertTitle), message: Text(viewModel.alertMessage), dismissButton: .default(Text("OK")))
                 }
-        }
-        
-    }
-    
-    
-    
-    
-    func getHiveSectionsArray() -> [HiveSection] {
-        var hiveSectionsArray: [HiveSection] = []
-        for (section, count) in hiveSections {
-            hiveSectionsArray += Array(repeating: section, count: count)
-        }
-        return hiveSectionsArray
-    }
-    func createHive() {
-        let hiveSectionsArray = getHiveSectionsArray()
-        
-        let hive = Hive(hiveNumber: hiveNumber, hiveProducent: selectedProducent, hiveSections: hiveSectionsArray)
-        
-        // Zapisz ul
-        let saveResult = Hive.saveHives([hive])
-        
-        if saveResult {
-            alertMessage = "Ul został pomyślnie utworzony."
-        } else {
-            alertMessage = "Wystąpił błąd podczas tworzenia ula."
-        }
-        showingAlert = true
-    }
-    
-    
-    func editHive(with hiveNumber: Int) {
-        if let index = Hive.loadHives().firstIndex(where: { $0.hiveNumber == hiveNumber }) {
-            let hiveSectionsArray = getHiveSectionsArray()
-            let updatedHive = Hive(hiveNumber: hiveNumber, hiveProducent: selectedProducent, hiveSections: hiveSectionsArray)
-            
-            // Wczytaj wszystkie ule, zastąp ul, który chcesz zaktualizować, i zapisz zmiany
-            var existingHives = Hive.loadHives()
-            existingHives[index] = updatedHive
-            let saveResult = Hive.saveHives(existingHives)
-            
-            if saveResult {
-                alertMessage = "Ul został pomyślnie zaktualizowany."
-            } else {
-                alertMessage = "Wystąpił błąd podczas aktualizacji ula."
-            }
-            showingAlert = true
-        } else {
-            print("Ul o numerze \(hiveNumber) nie został znaleziony.")
         }
     }
     
     struct CreateHiveView_Previews: PreviewProvider {
         static var previews: some View {
-            CreateHiveView(hiveNumber: 1, selectedProducent: .langstroth)
+            CreateHiveView()
         }
     }
-    
 }
---
+++ Beekeeper/View/Hive/CreateHiveViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  CreateHiveViewModel.swift
+//  Beekeeper
+//
+//  Created by Mariusz Zając on 25/08/2023.
+//
+
+import Foundation
+class CreateHiveViewModel: ObservableObject {
+    @Published var hiveNumber: Int = 0
+    @Published var selectedProducent: HiveProducent = .wielkopolski18
+    @Published var hiveSections: [HiveSection: Int] = [
+        .nest: 1,
+        .body: 0,
+        .honeySuper: 0,
+        .feeder: 0
+    ]
+    @Published var showingAlert = false
+    @Published var alertTitle = ""
+    @Published var alertMessage = ""
+    
+    func getHiveSectionsArray() -> [HiveSection] {
+        var hiveSectionsArray: [HiveSection] = []
+        for (section, count) in hiveSections {
+            hiveSectionsArray += Array(repeating: section, count: count)
+        }
+        return hiveSectionsArray
+    }
+    fileprivate func showMassageForResult(_ saveResult: Bool) {
+        if saveResult {
+            alertMessage = "Ul został pomyślnie utworzony."
+        } else {
+            alertMessage = "Wystąpił błąd podczas tworzenia ula."
+        }
+    }
+    
+    func createHive() {
+        let hiveSectionsArray = getHiveSectionsArray()
+        
+        let hive = Hive(hiveNumber: hiveNumber, hiveProducent: selectedProducent, hiveSections: hiveSectionsArray)
+        
+        // Zapisz ul
+        let saveResult = Hive.saveHives([hive])
+        showMassageForResult(saveResult)
+        showingAlert = true
+    }
+    
+    
+    func editHive(with hiveNumber: Int) {
+        if let index = Hive.loadHives().firstIndex(where: { $0.hiveNumber == hiveNumber }) {
+            let hiveSectionsArray = getHiveSectionsArray()
+            let updatedHive = Hive(hiveNumber: hiveNumber, hiveProducent: selectedProducent, hiveSections: hiveSectionsArray)
+            
+            // Wczytaj wszystkie ule, zastąp ul, który chcesz zaktualizować, i zapisz zmiany
+            var existingHives = Hive.loadHives()
+            existingHives[index] = updatedHive
+            let saveResult = Hive.saveHives(existingHives)
+            
+            showMassageForResult(saveResult)
+        } else {
+            print("Ul o numerze \(hiveNumber) nie został znaleziony.")
+        }
+    }
+    
+}
